66dcf2632dfa766ac780cce387fffbb8ad984f64
Frode Nordahl <frode.nordahl@canonical.com>
2020-02-17 14:45:34 +0100

    Enable bionic/ussuri and focal
    __NOTE__: This patch set contains a wheelhouse override to pull
    in and help validate in-flight changes. The override must be
    removed prior to actually landing the change.
    
    dep: https://github.com/juju/charm-helpers/pull/432
    dep: https://review.opendev.org/#/c/708566/
    
    Change-Id: I9a4a8dc422b8bdcf61b457f7b3ba828008c415d8

--- src/reactive/ovn_central_handlers.py
+++ src/reactive/ovn_central_handlers.py
@@ -27,6 +27,7 @@ charms_openstack.bus.discover()
 charm.use_defaults(
     'charm.installed',
     'config.changed',
+    'charm.default-select-release',
     'update-status',
     'upgrade-charm',
 )
@@ -140,6 +141,17 @@ def publish_addr_to_clients():
         if not ep:
             continue
         ep.publish_cluster_local_addr(ovsdb_peer.cluster_local_addr)
+
+
+@reactive.when_none('run-default-update-status')
+@reactive.when('config.changed.source', 'ovsdb-peer.available')
+def maybe_do_upgrade():
+    ovsdb_peer = reactive.endpoint_from_flag('ovsdb-peer.available')
+    with charm.provide_charm_instance() as ovn_charm:
+        ovn_charm.configure_source()
+        ovn_charm.upgrade_if_available([ovsdb_peer])
+        reactive.clear_flag('config.changed.source')
+        ovn_charm.assess_status()
 
 
 @reactive.when_none('run-default-update-status')
